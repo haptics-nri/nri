6efea26d970b318b4183f86d6753ad002995bdcc
Alex Burka <aburka@seas.upenn.edu>
2015-06-08 11:18:57 -0400

    web: templating via handlebars

--- src/web/mod.rs
+++ src/web/mod.rs
@@ -1,7 +1,12 @@
 extern crate iron;
+extern crate handlebars_iron as hbs;
 
+use std::path::Path;
 use std::sync::mpsc::{Receiver, TryRecvError};
 use super::comms::Cmd;
+use self::iron::prelude::*;
+use self::iron::status;
+use self::hbs::{Template, HandlebarsEngine};
 
 pub fn go(rx: Receiver<Cmd>) {
     match rx.recv() {
@@ -11,6 +16,16 @@ pub fn go(rx: Receiver<Cmd>) {
         },
         Err(e) => return, // main thread exploded?
     }
+
+    let mut chain = Chain::new(|_: &mut Request| {
+        let mut resp = Response::new();
+        resp.set_mut(Template::new("index", {})).set_mut(status::Ok);
+        Ok(resp)
+    });
+
+    chain.link_after(HandlebarsEngine::new(Path::new(file!()).parent().unwrap().join("templates").to_str().unwrap(), ".hbs"));
+
+    let mut listening = Iron::new(chain).http("localhost:3000").unwrap();
 
     loop {
         match rx.try_recv() {
@@ -24,5 +39,7 @@ pub fn go(rx: Receiver<Cmd>) {
             },
         }
     }
+
+    listening.close().unwrap(); // FIXME this does not do anything (known bug in hyper)
 }
 
