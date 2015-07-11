2e6cc7ea8c16f4d14c5af4ab90f44f6b0a41c0d6
Haptics NUC <hapticsnuc@alexburka.com>
2015-07-11 12:33:34 -0400

    scripts

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH=src/bluefox:src/optoforce
+export RUST_BACKTRACE=1
+
+if [ "$#" -ne 0 ]; then 
+    DEV=$1
+    shift
+    rlwrap cargo run --example read$DEV -- "$@"
+else
+    rlwrap cargo run
+fi
+
