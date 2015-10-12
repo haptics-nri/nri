1316d7897e61936b914af526baf6686c314657e1
Alex Burka <aburka@seas.upenn.edu>
2015-10-11 21:51:36 -0400

    printf debugging

--- examples/readbluefox.rs
+++ examples/readbluefox.rs
@@ -30,15 +30,16 @@ fn main() {
     csvwtr.encode(("Frame number", "Filename", "Unix timestamp"));
 
     const N_THREADS: usize = 4;
+    print!("Creating {} threads...", N_THREADS);
     let mut threads: [Option<(thread::JoinHandle<()>, mpsc::Sender<PathBuf>)>; N_THREADS] = unsafe { mem::uninitialized() };
     for i in 0..N_THREADS {
+        print!("{}...", i);
         let (tx, rx) = mpsc::channel::<PathBuf>();
         threads[i] = Some((
             thread::spawn(move || {
                 for dat_path in rx {
                     let dat = dat_path.to_str().unwrap().to_string();
                     let png = dat_path.with_extension("png").to_str().unwrap().to_string();
-                    indentln!("parsing {} into {}", dat, png);
                     let rows = common::do_binary::<Row>("", (dat, None));
                     let mut pixels = Vec::with_capacity(1200*3*rows.len());
                     for i in 0..rows.len() {
@@ -52,11 +53,12 @@ fn main() {
             tx
         ));
     }
+    println!("done!");
 
     let mut i = 0;
     let mut t = 0;
     for row in csvrdr.decode() {
-        indentln!(> "reading frame {}...", i);
+        println!("reading frame {}...", i);
         let (num, fname, stamp): (usize, String, f64) = row.ok().expect(&format!("failed to parse row {} of {}", i, inname));
         csvwtr.encode((num, Path::new(&fname).with_extension("png").to_str().unwrap().to_string(), stamp));
         i += 1;
@@ -64,7 +66,7 @@ fn main() {
         threads[t].as_ref().unwrap().1.send(dat_path);
         t = (t + 1) % 4;
     }
-    indentln!("finished {} frames", i);
+    println!("finished {} frames", i);
 
     for t in 0..N_THREADS {
         attempt!(threads[t].take().unwrap().0.join());
