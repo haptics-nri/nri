f248207ef42e224c21c71047e9f84b87099cf963
Alex Burka <aburka@seas.upenn.edu>
2015-10-11 22:35:19 -0400

    factor out readstructure

--- examples/readstructure.rs
+++ examples/readstructure.rs
@@ -1,14 +1,7 @@
 #[macro_use] extern crate lazy_static;
-extern crate csv;
-extern crate lodepng;
-
 #[macro_use] mod common;
 
-use std::{env, process, fmt};
-use std::io::{Read, Write};
-use std::fs::File;
-use std::path::Path;
-use lodepng::{encode_file, ColorType};
+use std::fmt;
 
 struct Row {
     pixels: [u16; 640]
@@ -24,37 +17,13 @@ impl fmt::Debug for Row {
     }
 }
 
-fn main() {
-    let inname = common::parse_in_arg(&mut env::args().skip(1));
-
-    let mut csvfile = attempt!(File::open(&inname));
-    let mut csvrdr = csv::Reader::from_reader(csvfile).has_headers(false);
-    let mut csvwtr = csv::Writer::from_memory();
-    csvwtr.encode(("Frame number", "Filename", "Unix timestamp"));
-
-    let mut i = 0;
-    for row in csvrdr.decode() {
-        indentln!(> "reading frame {}...", i);
-        let (num, fname, stamp): (usize, String, f64) = row.ok().expect(&format!("failed to parse row {} of {}", i, inname));
-        csvwtr.encode((num, Path::new(&fname).with_extension("png").to_str().unwrap().to_string(), stamp));
-        i += 1;
-        let dat_path = Path::new(&inname).with_file_name(fname);
-        let dat = dat_path.to_str().unwrap().to_string();
-        let png = dat_path.with_extension("png").to_str().unwrap().to_string();
-        indentln!("parsing {} into {}", dat, png);
-        let rows = common::do_binary::<Row>("", (dat, None));
-        indentln!("have {} rows", rows.len());
-        let mut pixels = Vec::with_capacity(640*rows.len());
-        for i in 0..rows.len() {
-            for j in 0..640 {
-                pixels.push(rows[i].pixels[j]);
-            }
-        }
-        println!("{} pixels!", pixels.len());
-        attempt!(encode_file(png, &pixels, 640, rows.len(), ColorType::LCT_GREY, 16));
+impl common::Pixels<u16> for Row {
+    fn pixel(&self, i: usize) -> u16 {
+        self.pixels[i]
     }
-    indentln!("finished {} frames", i);
+}
 
-    attempt!(File::create(&inname)).write_all(csvwtr.as_bytes());
+fn main() {
+    common::do_camera::<u16, Row>(640, 480, 1);
 }
 
