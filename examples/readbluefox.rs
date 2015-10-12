4450eede118ffb68439828dec7a6cd65e2ed7a2c
Alex Burka <aburka@seas.upenn.edu>
2015-10-11 21:38:51 -0400

    parallelize readbluefox

--- examples/readbluefox.rs
+++ examples/readbluefox.rs
@@ -4,10 +4,11 @@ extern crate lodepng;
 
 #[macro_use] mod common;
 
-use std::{env, process, fmt, mem, slice};
+use std::{env, thread, process, fmt, mem, slice};
+use std::sync::mpsc;
 use std::io::{Read, Write};
 use std::fs::File;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use lodepng::{encode_file, ColorType};
 
 struct Row {
@@ -28,28 +29,46 @@ fn main() {
     let mut csvwtr = csv::Writer::from_memory();
     csvwtr.encode(("Frame number", "Filename", "Unix timestamp"));
 
+    const N_THREADS: usize = 4;
+    let mut threads: [Option<(thread::JoinHandle<()>, mpsc::Sender<PathBuf>)>; N_THREADS] = unsafe { mem::uninitialized() };
+    for i in 0..N_THREADS {
+        let (tx, rx) = mpsc::channel::<PathBuf>();
+        threads[i] = Some((
+            thread::spawn(move || {
+                for dat_path in rx {
+                    let dat = dat_path.to_str().unwrap().to_string();
+                    let png = dat_path.with_extension("png").to_str().unwrap().to_string();
+                    indentln!("parsing {} into {}", dat, png);
+                    let rows = common::do_binary::<Row>("", (dat, None));
+                    let mut pixels = Vec::with_capacity(1200*3*rows.len());
+                    for i in 0..rows.len() {
+                        for j in 0..1600 {
+                            pixels.push(rows[i].pixels[j]);
+                        }
+                    }
+                    attempt!(encode_file(png, &pixels, 1600, rows.len(), ColorType::LCT_RGB, 8));
+                }
+            }),
+            tx
+        ));
+    }
+
     let mut i = 0;
+    let mut t = 0;
     for row in csvrdr.decode() {
         indentln!(> "reading frame {}...", i);
         let (num, fname, stamp): (usize, String, f64) = row.ok().expect(&format!("failed to parse row {} of {}", i, inname));
         csvwtr.encode((num, Path::new(&fname).with_extension("png").to_str().unwrap().to_string(), stamp));
         i += 1;
         let dat_path = Path::new(&inname).with_file_name(fname);
-        let dat = dat_path.to_str().unwrap().to_string();
-        let png = dat_path.with_extension("png").to_str().unwrap().to_string();
-        indentln!("parsing {} into {}", dat, png);
-        let rows = common::do_binary::<Row>("", (dat, None));
-        indentln!("have {} rows", rows.len());
-        let mut pixels = Vec::with_capacity(1200*3*rows.len());
-        for i in 0..rows.len() {
-            for j in 0..1600 {
-                pixels.push(rows[i].pixels[j]);
-            }
-        }
-        println!("{} pixels!", pixels.len());
-        attempt!(encode_file(png, &pixels, 1600, rows.len(), ColorType::LCT_RGB, 8));
+        threads[t].as_ref().unwrap().1.send(dat_path);
+        t = (t + 1) % 4;
     }
     indentln!("finished {} frames", i);
+
+    for t in 0..N_THREADS {
+        attempt!(threads[t].take().unwrap().0.join());
+    }
 
     attempt!(File::create(&inname)).write_all(csvwtr.as_bytes());
 }
