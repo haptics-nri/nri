17b11940fcab8e56e88a4723fb5a742d28f0a177
Haptics NUC <hapticsnuc@alexburka.com>
2017-08-31 19:31:22 -0400

    transfer: process multipe dirs from cmdline

--- src/bin/transfer.rs
+++ src/bin/transfer.rs
@@ -91,153 +91,157 @@ fn try_main() -> Result<()> {
         (author: crate_authors!("\n"))
         (about: "Helper for processing/transferring dataset files")
 
-        (@arg EPDIR: +required "Episode directory")
+        (@arg EPDIR: +required +multiple "Episode directory")
         (@arg DEST: +required "Destination (path or SSH address)")
     }.get_matches();
 
-    let epdir = Path::new(matches.value_of("EPDIR").unwrap()).to_owned();
-    let dest = matches.value_of("DEST").unwrap().to_owned();
+    for epdir in matches.values_of("EPDIR").unwrap() {
+        println!("Processing {}...", epdir);
 
-    println!("Checking arguments...");
+        let epdir = Path::new(epdir).to_owned();
+        let dest = matches.value_of("DEST").unwrap().to_owned();
 
-    /* check epdir is a real directory */
-    check_dir(&epdir)?;
+        println!("Checking arguments...");
 
-    /* check that dest is a dir or matches username:pw@hostname:dir */
-    let scp_regex = Regex::new(r"^(?P<user>[^:]+):(?P<pass>[^@]+)@(?P<host>[^:]+):(?P<dir>.*)$").unwrap();
-    let ssh_info = match scp_regex.captures(&dest) {
-        Some(captures) => {
-            let info = SshInfo {
-                user: captures.name("user").unwrap().as_str().to_owned(),
-                pass: captures.name("pass").unwrap().as_str().to_owned(),
-                host: captures.name("host").unwrap().as_str().to_owned(),
-                dir: PathBuf::from(captures.name("dir").unwrap().as_str()),
-            };
-            check_scp(info.clone())?;
-            Some(info)
-        }
-        None => {
-            check_dir(&dest)?;
-            None
-        }
-    };
+        /* check epdir is a real directory */
+        check_dir(&epdir)?;
 
-    /* process data if necessary */
-    let dats = glob(&epdir, "*.dat")?;
-    if dats.len() != 0 {
-        /* ./run.sh all $epdir */
-        println!("Processing data...");
-        let status = Command::new("./run.sh")
-            .arg("all").arg(&epdir)
-            .current_dir(env!("CARGO_MANIFEST_DIR"))
-            .status().chain_err(|| Io("run", "run.sh".into()))?;
-        if !status.success() {
-            bail!("run.sh failed");
-        }
-    }
+        /* check that dest is a dir or matches username:pw@hostname:dir */
+        let scp_regex = Regex::new(r"^(?P<user>[^:]+):(?P<pass>[^@]+)@(?P<host>[^:]+):(?P<dir>.*)$").unwrap();
+        let ssh_info = match scp_regex.captures(&dest) {
+            Some(captures) => {
+                let info = SshInfo {
+                    user: captures.name("user").unwrap().as_str().to_owned(),
+                    pass: captures.name("pass").unwrap().as_str().to_owned(),
+                    host: captures.name("host").unwrap().as_str().to_owned(),
+                    dir: PathBuf::from(captures.name("dir").unwrap().as_str()),
+                };
+                check_scp(info.clone())?;
+                Some(info)
+            }
+            None => {
+                check_dir(&dest)?;
+                None
+            }
+        };
 
-    /* check PNG/CSV/DAT file counts */
-    println!("Checking file counts...");
-    let dats  = glob(&epdir, "*.dat")?;
-    let flows = glob(&epdir, "*.flow")?;
-    let pngs  = glob(&epdir, "*.png")?;
-    let csvs  = glob(&epdir, "*.csv")?;
-    let bcsvs = glob(&epdir, "biotac.csv")?;
-    let ocsvs = glob(&epdir, "optoforce.csv")?;
-    println!("{} pngs, {} csvs, {} dats", pngs.len(), csvs.len(), dats.len());
-    if csvs.len() != 9*flows.len() + bcsvs.len() + ocsvs.len() {
-        bail!("Wrong number of CSV files!");
-    }
-    if dats.len() > 0 && pngs.len() != dats.len() - bcsvs.len() - ocsvs.len() - 2*flows.len() {
-        bail!("Wrong number of PNG files!");
-    }
-
-    /* find $epdir -name '*.dat' -exec rm {} \; */
-    if dats.len() > 0 {
-        println!("Deleting *.dat files...");
-        for dat in dats {
-            fs::remove_file(&dat).chain_err(|| Io("delete", dat))?;
-        }
-    }
-
-    /* check disk space */
-    let size = local_du(&epdir)?;
-    let free = ssh_info.clone().map(remote_df).unwrap_or_else(|| local_df(Path::new(&dest)))?;
-    println!("{} required, {} free", size, free);
-    if size.0 > free.0 {
-        bail!("not enough space at destination");
-    }
-
-    /* rsync -avh $epdir $dest (with progress bars!) */
-    println!("Rsyncing...");
-    let rsync_args = &["-avh"];
-    let mut cmd = if let Some(ssh) = ssh_info.clone() {
-        let mut cmd = Command::new("sshpass");
-        cmd.arg("-p").arg(ssh.pass)
-           .arg("rsync")
-           .args(rsync_args)
-           .arg(&epdir)
-           .arg(format!("{}@{}:{}", ssh.user, ssh.host, ssh.dir.display()));
-        cmd
-    } else {
-        let mut cmd = Command::new("rsync");
-        cmd.args(rsync_args)
-           .arg(&epdir)
-           .arg(&dest);
-        cmd
-    };
-    cmd.stdout(Stdio::piped()); // capture stdout for the progress bar
-    let mut child = cmd.spawn().chain_err(|| Io("run", "rsync".into()))?;
-
-    let bars = MultiProgress::new();
-    bars.set_move_cursor(true);
-    let num_bar = bars.add(make_bar((flows.len() + pngs.len() + csvs.len()) as u64)); // the first progress bar counts files transferred/evaluated
-    let size_bar = bars.add(make_bar_bytes(size.0 as u64)); // the second progress bar measures disk space
-    num_bar.set_message("Files");
-    size_bar.set_message("Bytes");
-
-    let num_progress = thread::spawn(move || -> Result<ExitStatus> {
-        num_bar.set_position(0);
-        for line in BufReader::new(child.stdout.as_mut().unwrap()).lines() {
-            let line = line.chain_err(|| Io("read output", "rsync".into()))?;
-            if line.ends_with(".flow") || line.ends_with(".png") || line.ends_with(".csv") {
-                num_bar.inc(1);
+        /* process data if necessary */
+        let dats = glob(&epdir, "*.dat")?;
+        if dats.len() != 0 {
+            /* ./run.sh all $epdir */
+            println!("Processing data...");
+            let status = Command::new("./run.sh")
+                .arg("all").arg(&epdir)
+                .current_dir(env!("CARGO_MANIFEST_DIR"))
+                .status().chain_err(|| Io("run", "run.sh".into()))?;
+            if !status.success() {
+                bail!("run.sh failed");
             }
         }
-        num_bar.finish();
 
-        // this shouldn't wait at all because the process is definitely done (it closed stdout)
-        Ok(child.wait().chain_err(|| Io("wait on", "rsync".into()))?)
-    });
-
-    let rsync_done = Arc::new(AtomicBool::new(false)); // signal for telling size progress bar to stop
-    let size_progress = thread::spawn(clone_army!([rsync_done, ssh_info] move || -> Result<()> {
-        size_bar.set_position(0);
-        while !rsync_done.load(atomic::Ordering::SeqCst) {
-            size_bar.set_position(ssh_info.clone()
-                                          .map(|SshInfo { user, pass, host, dir }| {
-                                              let mut dir = PathBuf::from(dir);
-                                              dir.push(epdir.file_name().unwrap()); // du -sh $REMOTE_DIR/$(basename $LOCAL_DIR)
-                                              remote_du(SshInfo { user, pass, host, dir })
-                                          })
-                                          .unwrap_or_else(|| local_du(Path::new(&dest)))?.0);
+        /* check PNG/CSV/DAT file counts */
+        println!("Checking file counts...");
+        let dats  = glob(&epdir, "*.dat")?;
+        let flows = glob(&epdir, "*.flow")?;
+        let pngs  = glob(&epdir, "*.png")?;
+        let csvs  = glob(&epdir, "*.csv")?;
+        let bcsvs = glob(&epdir, "biotac.csv")?;
+        let ocsvs = glob(&epdir, "optoforce.csv")?;
+        println!("{} pngs, {} csvs, {} dats", pngs.len(), csvs.len(), dats.len());
+        if csvs.len() != 9*flows.len() + bcsvs.len() + ocsvs.len() {
+            bail!("Wrong number of CSV files!");
         }
-        size_bar.finish();
-        Ok(())
-    }));
+        if dats.len() > 0 && pngs.len() != dats.len() - bcsvs.len() - ocsvs.len() - 2*flows.len() {
+            bail!("Wrong number of PNG files!");
+        }
 
-    // and one thread to bind them
-    let progress = thread::spawn(move || -> Result<()> {
-        bars.join().chain_err(|| Io("draw", "progress bar".into()))
-    });
+        /* find $epdir -name '*.dat' -exec rm {} \; */
+        if dats.len() > 0 {
+            println!("Deleting *.dat files...");
+            for dat in dats {
+                fs::remove_file(&dat).chain_err(|| Io("delete", dat))?;
+            }
+        }
 
-    let status = num_progress.join().unwrap()?; // first thread exits when rsync does
-    rsync_done.store(true, atomic::Ordering::SeqCst); // signal second thread to exit
-    size_progress.join().unwrap()?; // wait until it does (could be in the middle of a du)
-    progress.join().unwrap()?; // progress bar drawer thread should exit right away
+        /* check disk space */
+        let size = local_du(&epdir)?;
+        let free = ssh_info.clone().map(remote_df).unwrap_or_else(|| local_df(Path::new(&dest)))?;
+        println!("{} required, {} free", size, free);
+        if size.0 > free.0 {
+            bail!("not enough space at destination");
+        }
 
-    if !status.success() {
-        bail!("rsync failed");
+        /* rsync -avh $epdir $dest (with progress bars!) */
+        println!("Rsyncing...");
+        let rsync_args = &["-avh"];
+        let mut cmd = if let Some(ssh) = ssh_info.clone() {
+            let mut cmd = Command::new("sshpass");
+            cmd.arg("-p").arg(ssh.pass)
+               .arg("rsync")
+               .args(rsync_args)
+               .arg(&epdir)
+               .arg(format!("{}@{}:{}", ssh.user, ssh.host, ssh.dir.display()));
+            cmd
+        } else {
+            let mut cmd = Command::new("rsync");
+            cmd.args(rsync_args)
+               .arg(&epdir)
+               .arg(&dest);
+            cmd
+        };
+        cmd.stdout(Stdio::piped()); // capture stdout for the progress bar
+        let mut child = cmd.spawn().chain_err(|| Io("run", "rsync".into()))?;
+
+        let bars = MultiProgress::new();
+        bars.set_move_cursor(true);
+        let num_bar = bars.add(make_bar((flows.len() + pngs.len() + csvs.len()) as u64)); // the first progress bar counts files transferred/evaluated
+        let size_bar = bars.add(make_bar_bytes(size.0 as u64)); // the second progress bar measures disk space
+        num_bar.set_message("Files");
+        size_bar.set_message("Bytes");
+
+        let num_progress = thread::spawn(move || -> Result<ExitStatus> {
+            num_bar.set_position(0);
+            for line in BufReader::new(child.stdout.as_mut().unwrap()).lines() {
+                let line = line.chain_err(|| Io("read output", "rsync".into()))?;
+                if line.ends_with(".flow") || line.ends_with(".png") || line.ends_with(".csv") {
+                    num_bar.inc(1);
+                }
+            }
+            num_bar.finish();
+
+            // this shouldn't wait at all because the process is definitely done (it closed stdout)
+            Ok(child.wait().chain_err(|| Io("wait on", "rsync".into()))?)
+        });
+
+        let rsync_done = Arc::new(AtomicBool::new(false)); // signal for telling size progress bar to stop
+        let size_progress = thread::spawn(clone_army!([rsync_done, ssh_info] move || -> Result<()> {
+            size_bar.set_position(0);
+            while !rsync_done.load(atomic::Ordering::SeqCst) {
+                size_bar.set_position(ssh_info.clone()
+                                              .map(|SshInfo { user, pass, host, dir }| {
+                                                  let mut dir = PathBuf::from(dir);
+                                                  dir.push(epdir.file_name().unwrap()); // du -sh $REMOTE_DIR/$(basename $LOCAL_DIR)
+                                                  remote_du(SshInfo { user, pass, host, dir })
+                                              })
+                                              .unwrap_or_else(|| local_du(Path::new(&dest)))?.0);
+            }
+            size_bar.finish();
+            Ok(())
+        }));
+
+        // and one thread to bind them
+        let progress = thread::spawn(move || -> Result<()> {
+            bars.join().chain_err(|| Io("draw", "progress bar".into()))
+        });
+
+        let status = num_progress.join().unwrap()?; // first thread exits when rsync does
+        rsync_done.store(true, atomic::Ordering::SeqCst); // signal second thread to exit
+        size_progress.join().unwrap()?; // wait until it does (could be in the middle of a du)
+        progress.join().unwrap()?; // progress bar drawer thread should exit right away
+
+        if !status.success() {
+            bail!("rsync failed");
+        }
     }
 
     println!("Done!");
