d365ffb83a23abe63f0a0ff45701c943c4526126
Alex Burka <alex@alexburka.com>
2017-10-26 17:38:01 -0400

    datalocs: add more checks and comments

--- src/bin/datalocs.rs
+++ src/bin/datalocs.rs
@@ -2,6 +2,8 @@
 #[macro_use] extern crate error_chain;
 extern crate csv;
 
+extern crate utils;
+
 error_chain! {
     errors {
         Io(op: &'static str, path: PathBuf) {
@@ -20,6 +22,9 @@ use std::collections::BTreeMap;
 use std::fs::DirEntry;
 use std::path::{Path, PathBuf};
 
+use utils::prelude::*;
+
+/// Run a callback for each directory inside another directory
 fn for_each_subdir<P: AsRef<Path>, F: FnMut(DirEntry) -> Result<()>>(path: P, mut func: F) -> Result<()> {
     let path = path.as_ref();
     for entry in path.read_dir().chain_err(|| Io("list", path.into()))? {
@@ -38,18 +43,22 @@ quick_main!(|| -> Result<i32> {
         (about: "Helper for tabulating data locations")
 
         (@arg SURFACES: +required "Surfaces CSV file")
-        (@arg DATADIR: +required +multiple "Dataset directory")
+        (@arg DATADIR: +required +multiple "Dataset directory (can be a directory or a Nickname=directory pair")
     }.get_matches();
 
+    // step 1: catalog the YYYYMMDD dirs in each datadir
+    
     let args = matches.values_of("DATADIR").unwrap();
     let mut datalocs = BTreeMap::<u32, _>::new();
     let mut nicknames = BTreeMap::new();
     for arg in args {
+        // extract nickname if present (otherwise set nickname to dirname)
         let mut sp = arg.splitn(2, '=');
         let nickname = sp.next().ok_or("empty argument")?;
         let datadir = sp.next().unwrap_or(nickname);
         nicknames.insert(nickname, datadir);
 
+        // scan the datadir
         for_each_subdir(datadir, |date| {
             if let Ok(date) = date.file_name().to_string_lossy().parse() {
                 datalocs.entry(date)
@@ -60,14 +69,16 @@ quick_main!(|| -> Result<i32> {
         })?;
     }
 
-    let mut good = vec![];
+    // step 2: check status of each YYYYMMDD
+
+    let mut good = vec![]; // dates with at least two locations and no missing episodes in any location
     let mut bad = vec![];
     for (date, dirs) in datalocs {
-        let mut ok = true;
-        let dirs = {
-            let mut contents = BTreeMap::new();
-            let mut longest = vec![];
-            for dir in dirs {
+        let mut ok = true; // will be falsified if we find an issue with this date
+        let dir_desc = {
+            let mut contents = BTreeMap::new(); // map from nickname to episodes from this date
+            let mut longest = vec![]; // longest list of episodes (in order to find out if any datadirs are missing episodes)
+            for dir in &dirs {
                 let path = Path::new(nicknames[dir]).join(date.to_string());
                 let mut eps = vec![];
                 for_each_subdir(path, |endeff| {
@@ -90,6 +101,7 @@ quick_main!(|| -> Result<i32> {
                         if eps == longest {
                             format!("{}({})", dir, eps.len())
                         } else {
+                            // datadirs with missing episodes get an exclamation point
                             ok = false;
                             format!("{}({}!)", dir, eps.len())
                         }
@@ -98,17 +110,21 @@ quick_main!(|| -> Result<i32> {
                     .join(", ")
         };
 
-        if ok {
-            good.push((date, dirs));
+        if ok && dirs.len() > 1 {
+            good.push((date, dir_desc));
         } else {
-            bad.push((date, dirs));
+            bad.push((date, dir_desc));
         }
     }
+
+    // step 3: print naughty/nice list
 
     println!("GOOD DATADIRS:");
     for (date, dirs) in good { println!("{}\t{}", date, dirs); }
     println!("\nBAD DATADIRS:");
     for (date, dirs) in bad { println!("{}\t{}", date, dirs); }
+
+    // step 4: parse CSV
 
     let surf_file = matches.value_of("SURFACES").unwrap();
     let surfaces = csv::Reader::from_file(surf_file)?
@@ -122,9 +138,16 @@ quick_main!(|| -> Result<i32> {
                                   row[11].clone())))                // loc 2
         .collect::<Result<Vec<_>>>()?;
 
+    // step 5: look for and print various problems:
+    //  - missing info about data location in the CSV (or reference to a datadir that wasn't passed
+    //    in on the command line)
+    //  - duplicate/incomplete entries in the CSV
+    //  - inaccurate info in the CSV (specifying a datadir that doesn't contain the data)
+
     println!("\nEXCEPTIONS:");
+    let mut episodes = BTreeMap::new();
     for (i, (name, stick, opto, bio, loc1, loc2)) in surfaces.into_iter().enumerate() {
-        let i = i+2;
+        let i = i+2; // google sheets is 1-based and has a header row
         let mut locs = vec![];
 
         if loc1.is_empty() && loc2.is_empty() {
@@ -142,18 +165,32 @@ quick_main!(|| -> Result<i32> {
                 locs.push(loc2);
             }
 
-            for loc in locs {
-                for (endeff, &(ref date, ref num)) in vec![("stick", &stick), ("opto", &opto), ("bio", &bio)] {
-                    if !date.is_empty() {
-                        if num.is_empty() {
-                            println!("Row {} ({}) has a {} date but no episode number", i, name, endeff);
-                        } else {
+            // check that the episode for each present end-effector exists on each specified datadir
+            for (endeff, &(ref date, ref num)) in vec![("stick", &stick), ("opto", &opto), ("bio", &bio)] {
+                if !date.is_empty() {
+                    if num.is_empty() {
+                        println!("Row {} ({}) has a {} date but no episode number", i, name, endeff);
+                    } else {
+                        episodes.entry((endeff, date.clone(), num.clone()))
+                                .and_modify(|dupes: &mut Vec<_>| {
+                                    let dupes_desc = dupes.iter()
+                                                          .map(|&(i, ref name)|
+                                                               format!("row {} ({})", i, name))
+                                                          .collect::<Vec<_>>().join(", ");
+                                    println!("Row {} ({}) refers to {}/{}cam/{} which was already used in {}",
+                                             i, name, date, endeff, num, dupes_desc);
+                                    dupes.push((i, name.clone()));
+                                })
+                                .or_insert_with(|| vec![(i, name.clone())]);
+
+                        for loc in &locs {
                             if let Some(dir) = nicknames.get(&loc[..]) {
                                 if !Path::new(dir).join(date)
                                                   .join(format!("{}cam", &endeff))
                                                   .join(num)
                                                   .is_dir() {
-                                    println!("Row {} ({}) claims {}/{}cam/{} is on {} but it isn't", i, name, date, endeff, num, loc);
+                                    println!("Row {} ({}) claims {}/{}cam/{} is on {} but it isn't",
+                                             i, name, date, endeff, num, loc);
                                 }
                             } else {
                                 println!("Row {} ({}) refers to unknown location {}", i, name, loc);
